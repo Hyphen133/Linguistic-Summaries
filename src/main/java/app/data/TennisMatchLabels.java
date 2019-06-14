e74748cff1ff0080ec82419070c8b66d8d33414b
PatrycjaSzczakowska <PatrycjaSzczakowska@gmail.com>
2019-06-14 21:33:07 +0200

    new values for sets

--- src/main/java/app/data/TennisMatchLabels.java
+++ src/main/java/app/data/TennisMatchLabels.java
@@ -13,10 +13,10 @@ public class TennisMatchLabels {
 
     enum GamesDifference {
         //max= 15, average 4.94
-        ALMOST_NONE("ALMOST NONE", new FallingFunction(1, 2)),
-        SMALL("SMALL", new TriangularFunction(1.5, 2.5, 4)),
+        ALMOST_NONE("ALMOST NONE", new FallingFunction(1, 2.5)),
+        SMALL("SMALL", new TriangularFunction(1.4, 2.5, 3.78)),
         NORMAL(" NORMAL", new TriangularFunction(3, 6, 8)),
-        BIG("BIG", new RisingFunction(7, 9));
+        BIG("BIG", new RisingFunction(6, 9));
 
         String name;
         CharacteristicFunction characteristicFunction;
@@ -45,8 +45,8 @@ public class TennisMatchLabels {
     enum Aces {
         //max=53 average 5.46
         ALMOST_NONE("ALMOST NONE", new FallingFunction(1, 2)),
-        FEW("FEW", new TriangularFunction(1.5, 2.5, 4.5)),
-        SEVERAL("SEVERAL", new TriangularFunction(4, 6, 9)),
+        FEW("FEW", new TriangularFunction(1.5, 2.5, 5)),
+        SEVERAL("SEVERAL", new TriangularFunction(3.8, 6, 9)),
         MANY("MANY", new RisingFunction(8, 14));
 
         String name;
@@ -77,7 +77,7 @@ public class TennisMatchLabels {
         //max=20 average 2.94
         ALMOST_NONE("ALMOST NONE", new FallingFunction(0.5, 1)),
         FEW("FEW", new TriangularFunction(0.5, 1.5, 3)),
-        SEVERAL("SEVERAL", new TriangularFunction(2, 3, 7)),
+        SEVERAL("SEVERAL", new TriangularFunction(2, 3, 9)),
         MANY("MANY", new RisingFunction(5, 10));
 
         String name;
@@ -107,8 +107,8 @@ public class TennisMatchLabels {
     public enum AverageSetDurationInMinutes {
         //max = 655 average 40.03
         VERY_SHORT("VERY SHORT", new FallingFunction(5, 15)),
-        SHORT("SHORT", new TriangularFunction(10, 25, 35)),
-        NORMAL("NORMAL", new TriangularFunction(30, 40, 50)),
+        SHORT("SHORT", new TriangularFunction(10, 25, 40)),
+        NORMAL("NORMAL", new TriangularFunction(28, 40, 53)),
         LONG("LONG", new TriangularFunction(45, 60, 120)),
         VERY_LONG("VERY LONG", new RisingFunction(80, 100));
 
@@ -139,10 +139,10 @@ public class TennisMatchLabels {
     enum SuccessfulBreakPointsPercentage {
         //max= 0.5 average 0.33
         VERY_LOW("VERY LOW", new FallingFunction(0.1, 0.2)),
-        LOW("LOW", new TriangularFunction(0.15, 0.25, 0.3)),
-        AVERAGE("AVERAGE", new TriangularFunction(0.28, 0.33, 0.4)),
+        LOW("LOW", new TriangularFunction(0.10, 0.25, 0.32)),
+        AVERAGE("AVERAGE", new TriangularFunction(0.28, 0.33, 0.45)),
         HIGH("HIGH", new TriangularFunction(0.35, 0.5, 0.65)),
-        VERY_HIGH("VERY HIGH", new RisingFunction(0.6, 0.8));
+        VERY_HIGH("VERY HIGH", new RisingFunction(0.55, 0.8));
 
         String name;
         CharacteristicFunction characteristicFunction;
@@ -172,8 +172,8 @@ public class TennisMatchLabels {
         //max = 1.0 average 0.71
         VERY_LOW("VERY LOW", new FallingFunction(0.1, 0.3)),
         LOW("LOW", new TriangularFunction(0.2, 0.3, 0.5)),
-        AVERAGE("AVERAGE", new TriangularFunction(0.45, 0.7, 0.8)),
-        HIGH("HIGH", new TriangularFunction(0.75, 0.82, 0.9)),
+        AVERAGE("AVERAGE", new TriangularFunction(0.4, 0.7, 0.8)),
+        HIGH("HIGH", new TriangularFunction(0.72, 0.82, 0.95)),
         VERY_HIGH("VERY HIGH", new RisingFunction(0.85, 0.92));
 
         String name;
@@ -202,10 +202,10 @@ public class TennisMatchLabels {
 
     enum SecondServeWonPointsPercentage {
         //max = 1.0 average 0.51
-        VERY_LOW("VERY LOW", new FallingFunction(0.1, 0.2)),
-        LOW("LOW", new TriangularFunction(0.18, 0.3, 0.4)),
-        AVERAGE("AVERAGE", new TriangularFunction(0.38, 0.51, 0.78)),
-        HIGH("HIGH", new TriangularFunction(0.75, 0.8, 0.9)),
+        VERY_LOW("VERY LOW", new FallingFunction(0.1, 0.25)),
+        LOW("LOW", new TriangularFunction(0.13, 0.3, 0.45)),
+        AVERAGE("AVERAGE", new TriangularFunction(0.35, 0.51, 0.78)),
+        HIGH("HIGH", new TriangularFunction(0.7, 0.8, 0.9)),
         VERY_HIGH("VERY HIGH", new RisingFunction(0.85, 0.90));
 
         String name;
@@ -234,10 +234,10 @@ public class TennisMatchLabels {
 
     enum ReturnWonPointsPercentage {
         //max=0.88 average 0.37
-        VERY_LOW("VERY LOW", new FallingFunction(0.1, 0.2)),
-        LOW("LOW", new TriangularFunction(0.13, 0.25, 0.32)),
-        AVERAGE("AVERAGE", new TriangularFunction(0.28, 0.37, 0.5)),
-        HIGH("HIGH", new TriangularFunction(0.45, 0.6, 0.75)),
+        VERY_LOW("VERY LOW", new FallingFunction(0.1, 0.23)),
+        LOW("LOW", new TriangularFunction(0.10, 0.25, 0.33)),
+        AVERAGE("AVERAGE", new TriangularFunction(0.25, 0.37, 0.52)),
+        HIGH("HIGH", new TriangularFunction(0.45, 0.6, 0.9)),
         VERY_HIGH("VERY HIGH", new RisingFunction(0.7, 0.8));
 
         CharacteristicFunction characteristicFunction;
@@ -266,9 +266,9 @@ public class TennisMatchLabels {
 
     enum FirstServicePercentage {
         //max = 1.0 average 0.61
-        VERY_LOW("VERY LOW", new FallingFunction(0.15, 0.25)),
-        LOW("LOW", new TriangularFunction(0.2, 0.35, 0.45)),
-        AVERAGE("AVERAGE", new TriangularFunction(0.42, 0.61, 0.75)),
+        VERY_LOW("VERY LOW", new FallingFunction(0.15, 0.3)),
+        LOW("LOW", new TriangularFunction(0.18, 0.35, 0.5)),
+        AVERAGE("AVERAGE", new TriangularFunction(0.4, 0.61, 0.78)),
         HIGH("HIGH", new TriangularFunction(0.7, 0.8, 0.9)),
         VERY_HIGH("VERY HIGH", new RisingFunction(0.85, 0.95));
 
